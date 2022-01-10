141ca76a3b8fec69915efe00e96998c477f9f788
Payam <payamv@google.com>
2022-01-09 22:00:00 -0800

    final touchup - removing normalize.css (will be included in our sass)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,3 @@
 require("prismjs/themes/prism-tomorrow.css");
 require("prismjs/plugins/command-line/prism-command-line.css");
-require("normalize.css/normalize.css");
 require("./src/css/prism.css");
