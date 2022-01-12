3af1fc1d5991eb5b38411bfda3d876cd5cb73320
Amin <amin.di.sam@gmail.com>
2022-01-12 16:49:39 -0500

    change default export

--- src/components/notice/notice.tsx
+++ src/components/notice/notice.tsx
@@ -1,13 +1,11 @@
 import React from "react";
-import Markdown from 'markdown-to-jsx';
+import Markdown from "markdown-to-jsx";
 
-const Notice = ({ children, type }: { children: any; type?: string }) => {
+export default function ({ children, type }: { children: any; type?: string }) {
   return (
     <div style={{ border: "2px solid dodgerblue" }}>
       <p>{type}</p>
       <Markdown>{children}</Markdown>
     </div>
   );
-};
-
-export default Notice;
+}
