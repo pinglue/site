be0441a97b7c6ab168c04da420d1c8c63909f61b
takayuki <takayuki.hiraishi@gmail.com>
2022-01-22 22:27:51 -0800

    all changed files caused by merging main

--- src/components/doc-page.tsx
+++ src/components/doc-page.tsx
@@ -5,14 +5,17 @@ import { MDXProvider } from "@mdx-js/react";
 import Toc from "./toc/toc";
 import Notice from "./notice/notice";
 import { Tab, Tabs } from "./tab/tabs";
+import HeadersList from "./headersList/headersList";
+import { H2, H3 } from "./headers/headers";
 
 const gridWrapper = {
   display: "grid",
-  gridTemplateColumns: "300px 1fr",
+  gridTemplateColumns: "300px 1fr 300px",
 };
 
-export default function ({ docName, title, body, slug }) {
-  const shortcodes = { Notice, Tab, Tabs };
+export default function ({ docName, title, body, slug, headings }) {
+  const shortcodes = { Notice, Tab, Tabs, h2: H2, h3: H3 };
+
   return (
     <div style={gridWrapper}>
       <Toc docName={docName} slug={slug} />
@@ -24,6 +27,7 @@ export default function ({ docName, title, body, slug }) {
           </MDXProvider>
         </div>
       </article>
+      <HeadersList headings={headings} />
     </div>
   );
 }
