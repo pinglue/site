1d88d96cb5f40fec59659f60d6fc38f445b15a81
takayuki <takayuki.hiraishi@gmail.com>
2022-02-14 23:01:52 -0800

    cli-doc adjusted

--- src/pages/__mocks/cli-doc.tsx
+++ src/pages/__mocks/cli-doc.tsx
@@ -1,105 +1,89 @@
-import classNames from "classnames";
-import React from "react";
+
+import React, { useEffect, useState } from "react";
+import { DocTitle } from "../../components/doc-title/doc-title";
+import { Header } from "../../components/header/header";
+import '../../scss/bundle.scss';
+import classNames from 'classnames';
+import { Helmet } from 'react-helmet';
+import DocToc from "../../components/toc/doc-toc";
+import HeadersList from "../../components/headersList/headersList";
+import { commandName } from "../../components/cli-doc/cli-doc-item";
 import { cmdMock } from "./cmd";
-import DocPageRes from "./doc-page-res";
+import CliDocBody from "../../components/cli-doc/cli-doc-body";
 
-type Props = {
-	version?: string;
-	description?: string;
-	commands?: {
-		command: string
-		description: string;
-		options?: {
-			flags: string;
-			description: string;
-		}[]
-		examples?: {
-			description: string;
-			command: string;
-		}[]
-		action?: string;
-	}[]
-}
 
-export default function (data: Props) {
-	data = cmdMock;
+export default function () {
+    const [showLeftSidebar, setShowLeftSidebar] = useState(false);
+    const [showSandbar, setShowSandbar] = useState(false);
 
-	const commandName = (s: string) => {
-		return s.replace(/\s*\[.*?\]/, '');
-	}
+    const headings = [
+        { depth: 2, value: commandName(cmdMock.command) },
+        ...cmdMock.commands.map(item => {
+            return {
+                depth: 2,
+                value: commandName(item.command)
+            }
+        })];
 
-	const headings = data.commands.map(item => { return {
-		depth: 2,
-		value: commandName(item.command)
-	}});
+    let timer: any;
+    const threasholdToChangeLayout = 768;
 
-	return (
-		<DocPageRes headings={headings}>
-			<h1>Commands (pinglue CLI)</h1>
-			<h2>API commands</h2>
-			{
-				data.commands.map((item, i) => (
-					<div
-						key={`${commandName(item.command)}-${i}`}
-						className={classNames({ 'mb-3': (data.commands.length - 1 > i) })}
-					>
-						<h3 id={commandName(item.command)}>
-							<b>
-								<code 
-								className="p-h font-bold"
-								style={{background: '#fbf2e9'}}
-								>{commandName(item.command)}</code>
-							</b>
-						</h3>
-						<h4>Usage</h4>
-						<pre className="shell">
-							<code>
-								npx pg {item.command}
-							</code>
-						</pre>
-						<p>{item.description}</p>
+    const windowResizeHandler = () => {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            if (window.innerWidth >= threasholdToChangeLayout && showLeftSidebar) {
+                setShowLeftSidebar(false);
+            }
+        }, 300);
+    }
 
-						<table className="table">
-							<thead>
-								<tr>
-									<th scope="col">Flags</th>
-									<th scope="col">Description</th>
-								</tr>
-							</thead>
-							<tbody>
-								{
-									item.options.map(option => (
-										<tr>
-											<td>{option.flags}</td>
-											<td>{option.description}</td>
-										</tr>
-									))
-								}
-							</tbody>
-						</table>
-						{
-							item.examples ? <>
-								<h4>Examples</h4>
-								{
-									item.examples.map((ex, i) => (
-										<div
-											key={`${commandName(item.command)}-ex-${i}`}
-											className={classNames({ 'mb-3': (item.examples.length - 1 > i) })}
-										>
-											<p>{ex.description}</p>
-											<pre>
-												<code>
-													npx pg {ex.command}
-												</code>
-											</pre>
-										</div>
-									))
-								}</> : null
-						}
+    useEffect(() => {
+        window.addEventListener('resize', windowResizeHandler, true);
+        return window.removeEventListener('resize', windowResizeHandler, true);
+    });
 
-					</div>
-				))
-			}
-		</DocPageRes>
-	);
+    return (
+        <>
+            <Helmet>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+            </Helmet>
+            <div className={classNames('d-md-flex ss-doc-wrapper', { 'show-left-sidebar': showLeftSidebar })}>
+                <div className="ss-doc-wrapper__overlay" onClick={() => setShowLeftSidebar(false)}></div>
+                <div className="ss-doc-wrapper__left-sidebar">
+                    <div className="ss-doc-wrapper__title-area pt-h pt-md-1 px-2">
+                        <div className="d-flex justify-content-between align-items-center">
+                            <DocTitle />
+                            <div className='ms-1 d-md-none'>
+                                <button className="btn s-icon-btn" style={{ paddingRight: 0 }} onClick={() => { setShowLeftSidebar(false); }}>
+                                    <i className='bi-x'></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <DocToc docName="pgweb" slug="test" />
+                </div>
+
+                <div style={{ flex: '1 1 auto' }}>
+                    <div className="ss-doc-wrapper__header py-h py-md-1 px-2">
+                        <Header onClickMenu={() => { setShowLeftSidebar(true) }} />
+                    </div>
+
+                    <div className="d-lg-flex ss-doc-wrapper__main">
+                        <div className="ss-doc-wrapper__body p-2">
+                            <div className="s-content">
+                                <CliDocBody {...cmdMock} />
+                            </div>
+                        </div>
+                        <div className="ss-doc-wrapper__right-sidebar p-2">
+                            <h6>In this article</h6>
+                            <HeadersList headings={headings} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="py-hq px-2 text-center ss-doc-footer">
+                © 2022 pinglue
+            </div>
+        </>
+    );
 }
